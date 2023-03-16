ae40feb48681f51acc4d8af41842b990a4e6a982
Alex <sasha-boriskov@yandex.ru>
2023-03-17 00:10:26 +0300

    Палиндром

---
+++ Task19/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write ("Введите  5-ое число> ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n<10000 || n>99999)
+    Console.WriteLine ("Некорретное число");
+else if (n/10000==n%10 && (n/1000)%10==(n/10)%10)
+    Console.WriteLine ("Палиндром!");
+else 
+    Console.WriteLine ("Не палиндром!");
